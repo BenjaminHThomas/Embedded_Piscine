c555b5c5963cc3b5ac804f5c953a9c7f47899308
Ben <benjamin.h.thomas1@gmail.com>
2025-03-04 13:34:05 +0100

    changed from hardware timer to busy wait

--- module01/ex00/ex00.c
+++ module01/ex00/ex00.c
@@ -1,19 +1,22 @@
 #include <avr/io.h>
-#define F_CPU 16000000
 
 #define LED_2 (1 << PB1)
 
+// Don't do this, it's just for an exercise
+void delay500(void) {
+	uint64_t count;
+
+	count = 0;
+	while (count < 100000UL)
+		++count;
+}
+
 int main(void) {
 	DDRB |= LED_2; // set as output
-	TCCR1B |= ((1 << CS10) | (1 << CS12)); // set up timer with prescalar 1024
 
-	// 16000000 / 1024 = 15625 ops per second
-	// 15625 / 2 = 7812 oscilate per hz
 	while (1) {
-		if (TCNT1 >= 7812) {
-			PORTB ^= LED_2;
-			TCNT1 = 0;
-		}
+		delay500();
+		PORTB ^= LED_2;
 	}
 	return 0;
 }
