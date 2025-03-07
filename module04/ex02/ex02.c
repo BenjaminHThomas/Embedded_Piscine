057f9e873a450855d3bbc837d174fe8b1dc0c2c9
Ben <benjamin.h.thomas1@gmail.com>
2025-03-07 15:21:56 +0100

    button 2 mapped to PCINT2

--- module04/ex02/ex02.c
+++ module04/ex02/ex02.c
@@ -18,7 +18,7 @@
 
 volatile uint8_t num = 0;
 
-void update_leds(int inc) {
+void update_leds(uint8_t inc) {
 	num += inc;
 	PORTB = num;
 	PORTB = num & ~(LED_4);
@@ -33,18 +33,20 @@ ISR(INT0_vect) {
 	EIFR |= (1 << INTF0);
 }
 
-ISR(INT1_vect) {
+ISR(PCINT2_vect) {
 	update_leds(-1);
 	_delay_ms(200);
-	EIFR |= (1 << INTF1);
+	PCIFR |= (1 << PCIF2);
 }
 
 void init(void) {
+	// button 1
 	EICRA |= (1 << ISC01) | (1 << ISC00); // button 1 realise triggers interrupt
-	EICRA |= (1 << ISC11) | (1 << ISC10); // button 2 realise triggers interrupt
+	EIMSK |= (1 << INT0); // enable INT0
 	
-	EIMSK |= (1 << INT0); // enable INT0 interrupt
-	EIMSK |= (1 << INT1); // enable INT1 interrupt
+	// button 2
+	PCICR |= (1 << PCIE2); // enable pin change interrupts
+	PCMSK2 |= (1 << PCINT20); // enable pin change on PCINT20 (XCK)
 
 	sei();
 	DDRB |= LED_1 | LED_2 | LED_3 | LED_4;
