07dbdd5e2b0f46a08e84724e35d890a4d73feaf4
Ben <benjamin.h.thomas1@gmail.com>
2025-03-12 15:34:25 +0100

    reading and printing, value unchanging

--- module06/ex01/src/ex01.c
+++ module06/ex01/src/ex01.c
@@ -9,54 +9,93 @@
 //• You must display the return values in a loop that repeats with a frequency that
 //meets the manufacturer’s recommendations
 
+// SDA = Serial Data Line
+// SCL = Serial Clock Line
+
 #include "uart.h"
-
-#define AHT20_ADDRESS 0x38
-#define AHT20_WRITE (AHT20_ADDRESS << 1) // set last bit to 0 = 0x70
-#define AHT20_READ ((AHT20_ADDRESS << 1) | 1) // set last bit to 1 = 0x71
-
-#define HEX "0123456789ABCDEF"
-
-// ex00
-void i2c_init(void);
-void i2c_start(void);
-void i2c_stop(void);
-void print_status(uint8_t status_code);
-
-// ex01
-void i2c_write(unsigned char data);
-void i2c_read(void);
-void print_hex_value(char c);
+#include "aht20.h"
 
 int main(void) {
 	uart_init(MYUBRR);
+	uart_tx_str("init i2c...\r\n");
 	i2c_init();
-	_delay_ms(40);
+	uart_tx_str("init aht20...\r\n");
+	aht20_init();
+	uart_tx_str("starting main loop...\r\n");
+	aht20_start_measure();
 	while (1) {
 		i2c_start();
-		i2c_write(0xAC);
-		_delay_ms(80);
-		i2c_read();
+		i2c_write(AHT20_READ);
+		// print_status();
+
+		i2c_read(); // state
+		// print_status();
+		i2c_read(); // humidity0
+		// print_status();
+		i2c_read(); // humidity1
+		// print_status();
+		i2c_read(); // humidity2
+		// print_status();
+		i2c_read(); // temp0
+		// print_status();
+		i2c_read(); // temp1
+		// print_status();
+		i2c_read(); // crc
+		// print_status();
+		
 		i2c_stop();
+		uart_tx_str("\r\n");
+		_delay_ms(100);
 	}
 	return 0;
 }
 
+void aht20_init(void) {
+	_delay_ms(40);
+	aht20_send_cmd(AHT20_INIT); // init
+	_delay_ms(10);
+}
+
+void aht20_start_measure(void) {
+	aht20_send_cmd(AHT20_TRIG_MES);
+	_delay_ms(80);
+}
+
+// void ath20_send_full_cmd(uint8_t cmd, uint8_t param1, uint8_t param2) {
+// 	i2c_start();
+// 	i2c_write(AHT20_WRITE);
+// 	i2c_write(cmd);
+// 	i2c_write(param1);
+// 	i2c_write(param2);
+// 	i2c_stop();
+// }
+
+void aht20_send_cmd(uint8_t cmd) {
+	i2c_start();
+	i2c_write(AHT20_WRITE);
+	i2c_write(cmd);
+	i2c_stop();
+}
+
+void aht20_read_cmd() {
+	i2c_start();
+	i2c_write(AHT20_READ);
+	i2c_read();
+	i2c_stop();
+}
+
 void i2c_write(unsigned char data) {
-	uint8_t status;
-	TWDR = data; // copy data in TWI data register
-	TWCR = (1 << TWEN) | (1 << TWINT); // enable TWI and clear interrupt flag
-	while(!(TWCR & (1 << TWINT))); // wait on write
-	status = TWSR & 0xF8;
-	print_status(status);
+	TWDR = data;  // copy data in TWI data register
+	TWCR = (1<<TWINT) | (1 << TWEN); // enable TWI and clear interrupt flag
+	while(!(TWCR & (1<<TWINT))); // wait for data to be sent
 }
 
 void i2c_read(void) {
-	TWCR = (1 << TWEN) | (1 << TWINT) | (1 << TWEA); // Enable TWI, generation of ack
-	while(!(TWCR&(1<<TWINT))); // wait on read finish
-	char result = TWDR;
+	TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN); 
+	while (!(TWCR & (1 << TWINT)));
+	volatile char result = TWDR;
 	print_hex_value(result);
-	uart_tx_str("\r\n");
+	uart_tx_str(" ");
 }
 
 void print_hex_value(char c) {
@@ -83,28 +122,19 @@ void i2c_init(void) {
 }
 
 void i2c_start(void) {
-	volatile uint8_t status;
-	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-	while (!(TWCR & (1 << TWINT))); // wait for response
-	status = TWSR & 0xF8; // get status from register
-	if (status != TW_START)
-		print_status(status);
-	TWDR = AHT20_WRITE; // send slave address & write bit
-	TWCR = (1 << TWINT) | (1 << TWEN);
-	while (!(TWCR & (1 << TWINT)));
-	status = TWSR & 0xF8;
-	if (status != TW_MT_SLA_ACK)
-		print_status(status);
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTA);
+	while (!(TWCR & (1<<TWINT)));
 }
 
 void i2c_stop(void) {
-	TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
-	while(TWCR & (1 << TWSTO)); // wait for stop condition to be executed
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+	while(!(TWCR & (1 << TWSTO))); // wait for stop condition to be executed
 }
 
-void print_status(uint8_t status_code)
+void print_status(void)
 {
-	switch (status_code) {
+	uint8_t status = TWSR & 0xF8;
+	switch (status) {
 		case TW_START:
 			uart_tx_str ("START acknowledge.\r\n");
 			break;
